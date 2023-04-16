e9c51b4e9a5eab412b24d7b59a8644731343410d
Rafael da Silva <rafagomes53@hotmail.com>
2023-04-16 18:53:50 -0300

    Pratica_0 Montando o Ambiente do FrameWork

---
+++ Pratica_0/stm32f103c8t6/uarthwfc/uarthwfc.d
@@ -0,0 +1,18 @@
+uarthwfc.o: uarthwfc.c miniprintf.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdarg.h \
+ /home/rafael/curso_alura_cpp/UFC/RTOS/Pratica_0/stm32f103c8t6/libopencm3/include/libopencm3/stm32/rcc.h \
+ /home/rafael/curso_alura_cpp/UFC/RTOS/Pratica_0/stm32f103c8t6/libopencm3/include/libopencm3/cm3/common.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdbool.h \
+ /home/rafael/curso_alura_cpp/UFC/RTOS/Pratica_0/stm32f103c8t6/libopencm3/include/libopencm3/stm32/memorymap.h \
+ /home/rafael/curso_alura_cpp/UFC/RTOS/Pratica_0/stm32f103c8t6/libopencm3/include/libopencm3/stm32/f1/memorymap.h \
+ /home/rafael/curso_alura_cpp/UFC/RTOS/Pratica_0/stm32f103c8t6/libopencm3/include/libopencm3/cm3/memorymap.h \
+ /home/rafael/curso_alura_cpp/UFC/RTOS/Pratica_0/stm32f103c8t6/libopencm3/include/libopencm3/stm32/f1/rcc.h \
+ /home/rafael/curso_alura_cpp/UFC/RTOS/Pratica_0/stm32f103c8t6/libopencm3/include/libopencm3/stm32/common/rcc_common_all.h \
+ /home/rafael/curso_alura_cpp/UFC/RTOS/Pratica_0/stm32f103c8t6/libopencm3/include/libopencm3/stm32/gpio.h \
+ /home/rafael/curso_alura_cpp/UFC/RTOS/Pratica_0/stm32f103c8t6/libopencm3/include/libopencm3/stm32/f1/gpio.h \
+ /home/rafael/curso_alura_cpp/UFC/RTOS/Pratica_0/stm32f103c8t6/libopencm3/include/libopencm3/stm32/common/gpio_common_all.h \
+ /home/rafael/curso_alura_cpp/UFC/RTOS/Pratica_0/stm32f103c8t6/libopencm3/include/libopencm3/stm32/usart.h \
+ /home/rafael/curso_alura_cpp/UFC/RTOS/Pratica_0/stm32f103c8t6/libopencm3/include/libopencm3/stm32/f1/usart.h \
+ /home/rafael/curso_alura_cpp/UFC/RTOS/Pratica_0/stm32f103c8t6/libopencm3/include/libopencm3/stm32/common/usart_common_f124.h \
+ /home/rafael/curso_alura_cpp/UFC/RTOS/Pratica_0/stm32f103c8t6/libopencm3/include/libopencm3/stm32/common/usart_common_all.h
